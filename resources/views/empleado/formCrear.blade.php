2db786aa634dc102467675aa2f26f52adedc5bf9
Camilo Rojas <154857405+JCBRojas@users.noreply.github.com>
2025-07-16 17:16:56 -0500

    carga de archivos

---
+++ resources/views/empleado/formCrear.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="{{route('storeEmpleado')}}" method="post">
+        @csrf
+        <label for="employee">Nombres</label>
+        <input type="text" name="name" class="form-control">
+
+        <label for="">Apellidos</label>
+        <input type="text" name="lastname" class="form-control">
+
+        <label for="email">Correo</label>
+        <input type="email" name="email" class="form-control">
+
+        <label for="Area">Area</label>
+        <select name="area_id" id="area_id">
+            <option value="1"> Areas</option>
+            <option value="1"> Areas</option>
+            <option value="1"> Areas</option>
+        </select>
+
+        <label for="rol">Rol</label>
+        <select name="role_id" id="role_id">
+            <option value="1"> Roles</option>
+            <option value="1"> Roles</option>
+            <option value="1"> Roles</option>
+        </select>
+
+        <button type="submit">Guardar</button>
+        
+    </form>
+    
+</body>
+</html>
